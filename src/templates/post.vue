9b174c747b278c0344e4e22543cebff51098f44f
Szymon Polaczy <thomas_frey@tutanota.com>
2024-04-24 22:49:52 +0200

    Automatic font scalling

--- src/templates/post.vue
+++ src/templates/post.vue
@@ -26,6 +26,17 @@ main {
   max-width: 100%;
 }
 
+h1 {
+  font-size: clamp(2em, 3vw, 3em);
+}
+
+h2 {
+  font-size: clamp(1.6em, 2.5vw, 2.5em);
+}
+
+h3 {
+  font-size: clamp(1.45em, 2.2vw, 2.17em);
+}
 </style>
 
 <page-query>
